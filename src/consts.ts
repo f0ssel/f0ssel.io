3ee1cb9d630b8332da7077817ca057f3cc673ef5
Garrett <garrett@glaredb.com>
2023-09-23 15:43:34 -0400

    init

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "f0ssel";
+export const SITE_DESCRIPTION = "f0ssel's website";
